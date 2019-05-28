1371afaefd793325bebb720470f099f5b326a687
Arlo Erdaka <arlo.erdaka@gmail.com>
2019-05-28 21:14:35 +0700

    re-layout register page, add class for register page button

--- TestingScrollView/RegisterButton.swift
+++ TestingScrollView/RegisterButton.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+import UIKit
+
+class registerButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    func setupButton() {
+        setShadow()
+        setTitleColor(.white, for: .normal)
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func setShadow(){
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0.0, height: 6.0)
+        layer.shadowRadius = 8
+        layer.shadowOpacity = 0.5
+        clipsToBounds = true
+        layer.masksToBounds = false
+    }
+}
